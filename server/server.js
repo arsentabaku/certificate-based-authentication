c4660441570ad2b34b6464cb594df2e0ec334179
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2023-03-01 16:07:27 +0100

    Handled certificate check on server side

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.get("/authenticate", (req, res) => {
     res.send(
       `Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`
     );
+  } else if (cert.subject) {
+    res
+      .status(403)
+      .send(
+        `Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here.`
+      );
+  } else {
+    res
+      .status(401)
+      .send(`Sorry, but you need to provide a client certificate to continue.`);
   }
 });
 
